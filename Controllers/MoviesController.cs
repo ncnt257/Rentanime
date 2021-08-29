95c48881671f6a08d1b8dffc1ed56fefea40aa43
Tung Nguyen <ncnt257@gmail.com>
2021-08-29 15:55:20 +0700

    Added Movie model with controller and view

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rentanime.Models;
+
+namespace Rentanime.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "僕だけがいない街"
+            };
+            return View(movie);
+        }
+    }
+}
