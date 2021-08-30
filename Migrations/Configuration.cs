6b70c3ecb0b0d8b6e95ec47a3571bbc96f91d550
Tung Nguyen <ncnt257@gmail.com>
2021-08-30 18:14:15 +0700

    Enabled migration

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Rentanime.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Rentanime.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Rentanime.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Rentanime.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Anime> Animes { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
