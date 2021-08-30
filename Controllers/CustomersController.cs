1e386f74856752158bc8f9c14a3390db73b39194
Tung Nguyen <ncnt257@gmail.com>
2021-08-30 14:59:09 +0700

    Created Customers view with Details

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rentanime.Models;
+using Rentanime.ViewModels;
+
+namespace Rentanime.Controllers
+{
+    public class CustomersController : Controller
+    {
+       
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+
+            var list = new CustomersViewModel();
+            return View(list);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = new CustomersViewModel().Customers.Find(c=>c.Id==id);
+
+            return View(customer);
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.SqlServer.Server;
+
+namespace Rentanime.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
